c3674eb76cde51c04c433696eaec48dc88d7e1e9
NYX-shell <nyx.law.official@gmail.com>
2026-08-02 01:44:44 +0800

    REFACT: utils.has_string()
    refind function

--- lua/mason-select/utils.lua
+++ lua/mason-select/utils.lua
@@ -1,15 +1,11 @@
 local M = {}
 function M.has_string(Table, String)
-  for index, _ in pairs(Table) do
-    local TableString = Table[index]
+  for _, TableString in ipairs(Table) do
     -- vim.print(vim.o.filetype)
     -- vim.print('"' .. lang .. '"')
 
-    if string.lower(TableString) == string.lower(String) then
-      return String
-    else
-      return nil
-    end
+    if string.lower(TableString) == string.lower(String) then return String end
   end
+  return nil
 end
 return M
