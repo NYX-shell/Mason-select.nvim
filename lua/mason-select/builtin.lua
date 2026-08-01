7114a1c9e648ad1cf13aed3d25fd426579b3c844
NYX-shell <nyx.law.official@gmail.com>
2026-08-02 03:35:33 +0800

    FEAT: builtin.available_packages_names(include_languages)

--- lua/mason-select/builtin.lua
+++ lua/mason-select/builtin.lua
@@ -1,5 +1,6 @@
-local mason = require "mason-select.mason_tools" -- Import Utility tools
-local utils = require "mason-select.utils"
+local mason = require("mason-select.mason_tools") -- Import Utility tools
+local utils = require("mason-select.utils")
+local config = require("mason-select.config")
 
 local M = {}
 
@@ -13,23 +14,59 @@ function M.filter(packages_name, filter)
     local filter_languages = false
 
     for _, value in pairs(filter.spec.categories) do
-      if utils.has_string(package.spec.categories, value) then filter_categories = true end
+      if utils.has_string(package.spec.categories, value) then
+        filter_categories = true
+      end
     end
     for _, value in pairs(filter.spec.languages) do
-      if utils.has_string(package.spec.languages, value) then filter_languages = true end
+      if utils.has_string(package.spec.languages, value) then
+        filter_languages = true
+      end
     end
 
-    if filter_categories and filter_languages then table.insert(filtered_packages_name, package_name) end
+    if filter_categories and filter_languages then
+      table.insert(filtered_packages_name, package_name)
+    end
   end
 
   return filtered_packages_name
+end
+
+function M.available_packages_names(include_languages)
+  local available_packages_name = mason.get_packages_name()
+  if next(available_packages_name) == nil then
+    vim.notify("Mason-registry: return \"nil\"\nCheck is Mason config correctly.", vim.log.levels.ERROR)
+    return nil
+  end
+
+  -- config filter && languages
+  local filter = config.options.filter
+  -- add runtime_filetype -> {filter_languages}
+  if config.options.auto == true then
+    table.insert(filter.spec.languages, vim.o.filetype)
+  end
+  -- merge {filter_languages} with arg1
+  if type(include_languages) == "string" then
+    table.insert(filter.spec.languages, include_languages)
+  end
+  if type(include_languages) == "table" then
+    vim.tbl_extend("force", filter.spec.languages, include_languages)
+  end
+
+  available_packages_name = M.filter(available_packages_name, filter)
+  if next(available_packages_name) == nil then
+    return nil
+  end
+  return available_packages_name
 end
 
 function M.select_package(packages_name)
   local package_name = ""
 
   vim.ui.select(packages_name, { prompt = "Support package(Mason)" }, function(item, _)
-    if item then package_name = item end
+    if item then
+      package_name = item
+    end
   end)
 
   if package_name == "" then
@@ -45,7 +82,9 @@ function M.select_action(package)
   if package:is_installed() then
     local actions = { "Reinstall", "Uninstall" }
     vim.ui.select(actions, { prompt = package.name .. " is installed, choose:" }, function(item, _)
-      if item then action = item end
+      if item then
+        action = item
+      end
     end)
   else
     action = "Install"
--- lua/mason-select/init.lua
+++ lua/mason-select/init.lua
@@ -1,30 +1,25 @@
 local mason = require "mason-select.mason_tools" -- Import Utility tools
 local builtin = require "mason-select.builtin"
-local config = require "mason-select.config"
 
 local M = {}
 
 -- vim.print() -- for debug
-M.open = function(languages)
-  local available_packages_name = mason.get_packages_name()
-
-  -- config filter && languages
-  local filter = config.options.filter
-  if config.options.auto == true then table.insert(filter.spec.languages, vim.o.filetype) end
-  if type(languages) == "string" then table.insert(filter.spec.languages, languages) end
-  if type(languages) == "table" then vim.tbl_extend("force", filter.spec.languages, languages) end
-
-  available_packages_name = builtin.filter(available_packages_name, filter)
-  if next(available_packages_name) == nil then
-    vim.print "Mason-select: no found available package"
+M.open = function(include_languages)
+  -- filter languages from opts and arg1
+  local available_packages_names = builtin.available_packages_names(include_languages)
+  if available_packages_names == nil then
+    vim.notify("Mason-select: no found available package", vim.log.levels.WARN)
     return
   end
 
-  local selected_package_name = builtin.select_package(available_packages_name)
+  -- call builtin selecter
+  local selected_package_name = builtin.select_package(available_packages_names)
   if not selected_package_name then return end
 
+  -- get mason package object
   local selected_package = mason.get_package(selected_package_name)
 
+  -- builtin action selecter
   local action = builtin.select_action(selected_package)
   if not action then return end
 
